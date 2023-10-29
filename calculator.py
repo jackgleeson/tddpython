f689c7d5377f7374009f06c7996f61152cc5e8db
jgleeson <jgleeson@wikimedia.org>
2023-10-29 17:17:38 +0000

    Update implementation

--- calculator.py
+++ calculator.py
@@ -1,18 +1,25 @@
 class Calculator:
-    @staticmethod
-    def add(augend, addend):
-        if not (isinstance(augend, (int, float)) and isinstance(addend, (int, float))):
-            raise TypeError("Both augend and addend must be of type int or float")
+
+    def add(self, augend, addend):
+        self._check_number_type(augend, addend)
         return augend + addend
 
-    @staticmethod
-    def subtract(minuend, subtrahend):
+    def subtract(self, minuend, subtrahend):
+        self._check_number_type(minuend, subtrahend)
         return minuend - subtrahend
 
-    @staticmethod
-    def multiply(multiplicand, multiplier):
+    def multiply(self, multiplicand, multiplier):
+        self._check_number_type(multiplicand, multiplier)
         return multiplicand * multiplier
 
-    @staticmethod
-    def divide(dividend, divisor):
+    def divide(self, dividend, divisor):
+        self._check_number_type(dividend, divisor)
+        if divisor == 0:
+            raise ZeroDivisionError("Division by zero is not allowed")
         return dividend / divisor
+
+    @staticmethod
+    def _check_number_type(*args):
+        for arg in args:
+            if not isinstance(arg, (int, float)):
+                raise TypeError("Arguments must be of type int or float")
